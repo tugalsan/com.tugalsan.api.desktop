e13f3f5f49db227fe1017c6acc4882be4f29cc34
tugalsan karabacak <tugalsan@gmail.com>
2023-01-13 06:54:43 +0300

    .

--- target/classes/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.tugalsan.api.desktop {
-    requires java.desktop;
-    requires com.tugalsan.api.pack;
-    requires com.tugalsan.api.shape;
-    requires com.tugalsan.api.executable;
-    requires com.tugalsan.api.unsafe;
-    exports com.tugalsan.api.desktop.server;
-}
