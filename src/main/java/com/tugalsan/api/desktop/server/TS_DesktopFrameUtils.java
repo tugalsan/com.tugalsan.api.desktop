46139d39655ddca94e9e224edd13a706819ce38a
tugalsan karabacak <tugalsan@gmail.com>
2023-04-24 11:12:45 +0300

    .

--- src/main/java/com/tugalsan/api/desktop/server/TS_DesktopFrameUtils.java
+++ src/main/java/com/tugalsan/api/desktop/server/TS_DesktopFrameUtils.java
@@ -3,8 +3,8 @@ package com.tugalsan.api.desktop.server;
 import com.tugalsan.api.runnable.client.TGS_Runnable;
 import com.tugalsan.api.shape.client.*;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class TS_DesktopFrameUtils {
 
@@ -48,4 +48,26 @@ public class TS_DesktopFrameUtils {
         SwingUtilities.invokeLater(() -> run.run());
     }
 
+    public static void setThemeDarkLAF(Component comp) {
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+            UIManager.put("control", new Color(128, 128, 128));
+            UIManager.put("info", new Color(128, 128, 128));
+            UIManager.put("nimbusBase", new Color(18, 30, 49));
+            UIManager.put("nimbusAlertYellow", new Color(248, 187, 0));
+            UIManager.put("nimbusDisabledText", new Color(128, 128, 128));
+            UIManager.put("nimbusFocus", new Color(115, 164, 209));
+            UIManager.put("nimbusGreen", new Color(176, 179, 50));
+            UIManager.put("nimbusInfoBlue", new Color(66, 139, 221));
+            UIManager.put("nimbusLightBackground", new Color(18, 30, 49));
+            UIManager.put("nimbusOrange", new Color(191, 98, 4));
+            UIManager.put("nimbusRed", new Color(169, 46, 34));
+            UIManager.put("nimbusSelectedText", new Color(255, 255, 255));
+            UIManager.put("nimbusSelectionBackground", new Color(104, 93, 156));
+            UIManager.put("text", new Color(230, 230, 230));
+            SwingUtilities.updateComponentTreeUI(comp);
+        } catch (UnsupportedLookAndFeelException exc) {
+            System.err.println("Nimbus: Unsupported Look and feel!");
+        }
+    }
 }
