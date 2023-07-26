82792c5c8e09568759784755f9b7d7f5222211f5
tugalsan karabacak <tugalsan@gmail.com>
2023-07-26 08:24:54 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.tugalsan.api.desktop {
     requires com.tugalsan.api.tuple;
     requires com.tugalsan.api.shape;
     requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.callable;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.cast;
     requires com.tugalsan.api.charset;
