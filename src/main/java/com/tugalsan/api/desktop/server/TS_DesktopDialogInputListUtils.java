91a7035da5204a546e09974ca13a4d3300b45847
tugalsan karabacak <tugalsan@gmail.com>
2023-08-16 09:27:49 +0300

    .

--- src/main/java/com/tugalsan/api/desktop/server/TS_DesktopDialogInputListUtils.java
+++ src/main/java/com/tugalsan/api/desktop/server/TS_DesktopDialogInputListUtils.java
@@ -1,10 +1,17 @@
 package com.tugalsan.api.desktop.server;
 
 import java.awt.Component;
+import java.util.List;
 import java.util.Optional;
 import javax.swing.JOptionPane;
 
 public class TS_DesktopDialogInputListUtils {
+
+    public static Optional<String> show(Component parent, String title, String message, int defaultIdx, List<String> options) {
+        return show(parent, title, message, defaultIdx,
+                options.stream().toArray(String[]::new)
+        );
+    }
 
     public static Optional<String> show(Component parent, String title, String message, int defaultIdx, String... options) {
         var result = JOptionPane.showInputDialog(
