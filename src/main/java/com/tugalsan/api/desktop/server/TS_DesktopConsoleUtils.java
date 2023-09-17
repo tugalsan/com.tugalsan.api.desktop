8ad1451a35ca232f63e3b4ff53a8eeb6706b4d18
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-17 16:49:07 +0300

    Update TS_DesktopConsoleUtils.java

--- src/main/java/com/tugalsan/api/desktop/server/TS_DesktopConsoleUtils.java
+++ src/main/java/com/tugalsan/api/desktop/server/TS_DesktopConsoleUtils.java
@@ -1,6 +1,10 @@
 package com.tugalsan.api.desktop.server;
 
+import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import java.io.BufferedReader;
 import java.io.Console;
+import java.io.InputStreamReader;
+import java.util.Optional;
 
 public class TS_DesktopConsoleUtils {
 
@@ -8,8 +12,22 @@ public class TS_DesktopConsoleUtils {
         return System.console();
     }
 
-    public static String readPassword(Console console, String msg) {
-        char[] passwordArray = console.readPassword(msg);
-        return new String(passwordArray);
+    public static Optional<String> readLine() {
+        return TGS_UnSafe.call(() -> {
+            if (System.console() != null) {
+                return Optional.of(System.console().readLine());
+            }
+            var reader = new BufferedReader(new InputStreamReader(System.in));
+            return Optional.of(reader.readLine());
+        }, e -> Optional.empty());
+    }
+
+    public static Optional<String> readPassword() {
+        return TGS_UnSafe.call(() -> {
+            if (System.console() != null) {
+                return Optional.of(new String(System.console().readPassword()));
+            }
+            return readLine();
+        }, e -> Optional.empty());
     }
 }
