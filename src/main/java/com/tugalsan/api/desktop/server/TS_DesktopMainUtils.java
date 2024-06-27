4e4658f3b4b3521f769e1f1c0427c8e46434e54b
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:09:31 +0300

    .

--- src/main/java/com/tugalsan/api/desktop/server/TS_DesktopMainUtils.java
+++ src/main/java/com/tugalsan/api/desktop/server/TS_DesktopMainUtils.java
@@ -2,19 +2,19 @@ package com.tugalsan.api.desktop.server;
 
 import java.awt.Component;
 import javax.swing.SwingUtilities;
-import com.tugalsan.api.callable.client.TGS_CallableType0;
-import com.tugalsan.api.callable.client.TGS_CallableType0_Run;
+import com.tugalsan.api.function.client.TGS_Func_OutTyped;
+import com.tugalsan.api.function.client.TGS_Func;
 
 public class TS_DesktopMainUtils {
 
-    public static void setThemeAndinvokeLaterAndFixTheme(TGS_CallableType0<Component> component) {
+    public static void setThemeAndinvokeLaterAndFixTheme(TGS_Func_OutTyped<Component> component) {
         TS_DesktopThemeUtils.setTheme();
         TS_DesktopMainUtils.invokeLater(() -> {
             TS_DesktopThemeUtils.setThemeDarkLAF(component.call());
         });
     }
 
-    public static void invokeLater(TGS_CallableType0_Run run) {
+    public static void invokeLater(TGS_Func run) {
         SwingUtilities.invokeLater(() -> run.run());
     }
 }
